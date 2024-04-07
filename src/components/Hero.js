11f92498cdb1f780d76b1aa695791845898a4c17
Melo La Mantia <carmelo.la.mantia00@gmail.com>
2024-04-07 13:13:29 +0200

    scroll bar edit

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -10,7 +10,8 @@ export default function Hero() {
       
         <div className="col-12 col-md-8 hero-content">
           <h2 className="mx-5 px-3">Welcome</h2>
-          <p>Sono un giovane sviluppatore full stack con esperienza nel mondo dei siti web e una passione particolare per il lato tecnico. Ho seguito un corso intensivo dove ho imparato tanto sul front end quanto sul back end, grazie a un approccio pratico basato sugli oggetti. Ho anche capito come funziona il metodo Agile Scrum durante vari esercizi . Oltre a saper programmare, ho anche sviluppato buone capacità di comunicazione, gestione dello stress e risoluzione dei problemi.</p>
+          {/* <p>Sono un giovane sviluppatore full stack con esperienza nel mondo dei siti web e una passione particolare per il lato tecnico. Ho seguito un corso intensivo dove ho imparato tanto sul front end quanto sul back end, grazie a un approccio pratico basato sugli oggetti. Ho anche capito come funziona il metodo Agile Scrum durante vari esercizi . Oltre a saper programmare, ho anche sviluppato buone capacità di comunicazione, gestione dello stress e risoluzione dei problemi.</p> */}
+          <p>I am a young full stack developer with experience in the world of websites and a particular passion for the server side. I took an intensive course where I learned as much about the front end as the back end, thanks to a practical object-based approach. I also understood how the Agile Scrum method works during various exercises. In addition to knowing how to code, I have also developed good communication, stress management and problem-solving skills.</p>
         </div>
 
         <div className="col-12 col-md-3 hero-img">
--- src/components/Vnavbar.js
+++ src/components/Vnavbar.js
@@ -55,4 +55,4 @@ export default function Vnavbar() {
       </nav>
     </>
   );
-}
+} 
